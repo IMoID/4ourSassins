7d63b73dea991dd8432cb76c478efc5b9c77f4ad
IMoID <isaacdchapman@gmail.com>
2026-01-27 21:08:16 -0600

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,11 @@
+from js import document
+
+canvas = document.getElementById("game")
+ctx = canvas.getContext("2d")
+
+ctx.fillStyle = "darkslategray"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+ctx.fillStyle = "gold"
+ctx.fillRect(100, 100, 100, 100)
 
