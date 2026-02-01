afcc2283284e231193c8f645c58fb2397b63c60c
IMoID <isaacdchapman@gmail.com>
2026-02-01 12:02:59 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
+// test object
+let x = 300;
+let y = 220;
+const size = 32;
+
+function draw() {
+  // clear screen
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // draw square
+  ctx.fillStyle = "gold";
+  ctx.fillRect(x, y, size, size);
+}
+
+// game loop
+setInterval(draw, 16);
