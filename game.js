7a992b6fc1378b6c70e6f7d766a74aec2635ccc5
IMoID <isaacdchapman@gmail.com>
2026-02-01 13:05:46 -0600

    movement game.js

--- game.js
+++ game.js
@@ -1,12 +1,29 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+let speed = 4;
+const keys = {};
+
+document.addEventListener("keydown", (e) => {
+  keys[e.key] = true;
+});
+
+document.addEventListener("keyup", (e) => {
+  keys[e.key] = false;
+});
+
 // test object
 let x = 300;
 let y = 220;
 const size = 32;
 
 function draw() {
+  // movement
+  if (keys["w"] || keys["ArrowUp"]) y -= speed;
+  if (keys["s"] || keys["ArrowDown"]) y += speed;
+  if (keys["a"] || keys["ArrowLeft"]) x -= speed;
+  if (keys["d"] || keys["ArrowRight"]) x += speed;
+
   // clear screen
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
